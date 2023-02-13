be7c946d6b42d66e2ff697fa4e53d39bfd265ca0
jchozas <juanchozass@gmail.com>
2023-02-13 09:14:11 +0100

    remove duplicate eslint config

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,16 @@
-import { fireEvent, render } from "@testing-library/react";
+import { screen, fireEvent, render } from "@testing-library/react";
 import App from "./App";
 
 test("displays no textinput on render", () => {
-  const { queryByTestId } = render(<App />);
-  const searchField = queryByTestId("search-field");
+  render(<App />);
+  const searchField = screen.queryByTestId("search-field");
   expect(searchField).toBeFalsy();
 });
 
 test("displays textinput when spacebar is pressed", async () => {
-  const { queryByTestId } = render(<App />);
+  render(<App />);
   fireEvent.keyUp(document, { code: "Space" });
 
-  const searchField = queryByTestId("search-field");
+  const searchField = screen.queryByTestId("search-field");
   expect(searchField).toBeInTheDocument();
 });
